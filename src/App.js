3328887042e042d5b1f76c49abcab8af3b1dc660
lpozo1990 <lpozo1990@gmail.com>
2021-10-28 10:47:00 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="app-title">Todo App</h1>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
